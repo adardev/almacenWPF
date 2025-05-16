69f64f81a10413496365d11e39272ba84dd9365e
22100190 <22100190@A22100190>
2025-05-16 03:52:38 -0600

    Add project files.

---
+++ almacenWPF/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using almacenWPF.Models;
+using Supabase;
+using System.Windows;
+using static Supabase.Postgrest.Constants;
+
+
+namespace almacenWPF {
+    public partial class MainWindow : Window {
+        private Client supabase;
+
+        public MainWindow() {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            var url = "https://yvhkwhirvzavpbpaxxrb.supabase.co";
+            var key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl2aGt3aGlydnphdnBicGF4eHJiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDczNjMwMDIsImV4cCI6MjA2MjkzOTAwMn0.7A9YtrIoS-W0tZ-q8VJnTPkiSCf6fhBtY046sSbltLs"; // tu clave
+
+            var options = new SupabaseOptions
+            {
+                AutoConnectRealtime = true
+            };
+
+            supabase = new Client(url, key, options);
+            await supabase.InitializeAsync();
+        }
+        
+        private async void login(object sender, RoutedEventArgs e)
+        {
+            string nombreUsuario = nombre.Text.Trim();
+            string contraseña = password.Password.Trim();
+
+            var response = await supabase
+                .From<Usuario>()
+                .Filter("nombre", Operator.Equals, nombreUsuario)
+                .Filter("password", Operator.Equals, contraseña)
+                .Get();
+
+            if (response.Models.Count > 0) {
+                MessageBox.Show("Inicio de sesión exitoso.");
+
+                Window1 dashboard = new Window1();
+                dashboard.Show();
+
+                this.Close(); // opcional: cierra la ventana de login
+
+            }
+            else
+            {
+                MessageBox.Show("Usuario o contraseña incorrectos.");
+            }
+        }
+        
+    }
+}
---
+++ almacenWPF/Usuario.cs
@@ -0,0 +1,27 @@
+﻿using Supabase.Postgrest.Attributes;
+using Supabase.Postgrest.Models;
+
+namespace almacenWPF.Models
+{
+    [Table("usuario")]
+    public class Usuario : BaseModel
+    {
+        [PrimaryKey("id_user", false)]
+        public int Id { get; set; }
+
+        [Column("nombre")]
+        public string Nombre { get; set; }
+
+        [Column("password")]
+        public string Password { get; set; }
+
+        [Column("apellidos")]
+        public string Apellidos { get; set; }
+
+        [Column("fecha_nac")]
+        public DateTime FechaNac { get; set; }
+
+        [Column("tipo")]
+        public string Tipo { get; set; }
+    }
+}
